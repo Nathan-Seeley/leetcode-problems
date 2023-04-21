9d9776eae69a1a6f91e63cbb25e5a68101ecf6a0
Nathan-Seeley <wntrstk111@gmail.com>
2023-04-21 09:29:41 -0400

    finished second problem, is a number a palindrome under easy

--- main.py
+++ main.py
@@ -12,7 +12,29 @@ class Solution(object):
             for index_b, value_b in enumerate(nums):
                 if value_a + value_b == target and index_a != index_b:
                     return [index_a, index_b]
+
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+
+        """
+
+        inputNum = x
+        newNum = 0
+        while x < 0:
+            if x<0:
+                return False
+            
+            while x > 0:
+                newNum = newNum * 10 + x % 10
+                x = x//10
+            return newNum == inputNum
+        
 my_solution = Solution()
 
-result = my_solution.twoSum([2,7,11,15],9)
-print(result)
+# result = my_solution.twoSum([2,7,11,15],9)
+# print(result)
+
+result = my_solution.isPalindrome(121)
+print (result)
