3fb4bd558852ae3e7897f6c48dfbb74cbbaed919
Nathan-Seeley <wntrstk111@gmail.com>
2023-05-02 08:33:38 -0400

    Starting new problem removeDuplicates

--- main.py
+++ main.py
@@ -104,37 +104,6 @@ class Solution(object):
                 return False
         return res == []
     
-    def mergeTwoLists(self, l1, l2):
-    
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        create dummy node to avoid any edge cases
-        set tail of list equal to dummy
-        Iterate through both lists with condition both are non-empty
-        Starting at the beginning of both lists compare the value from list one to list 2
-        insert the lesser of the two values into the output
-        then move to the next value
-        insert remaininder list into new list
-        """
-        dummy = ListNode()
-        tail = dummy
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
-        if l1:
-            tail.next = l1
-        elif l2:
-            tail.next = l2
-        return dummy.next
-
 my_solution = Solution()
 
 # result = my_solution.twoSum([2,7,11,15],9)
@@ -152,6 +121,3 @@ my_solution = Solution()
 
 # result = my_solution.isValid("()")
 # print (result)
-
-result = my_solution.mergeTwoLists([1,2,4],[1,3,4])
-print (result)
