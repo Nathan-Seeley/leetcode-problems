bb16e9a31f5e5c40b9558487a35be146b117124a
Nathan-Seeley <wntrstk111@gmail.com>
2023-06-02 20:43:58 -0400

    Tested solution to completion

--- main.py
+++ main.py
@@ -308,8 +308,15 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+        while n > 0:
+            if nums1[m-1] >= nums2[n-1] and m>0:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1]= nums2[n-1]
+                n -= 1
+        return nums1
 
-        
 
 my_solution = Solution()
 
@@ -355,3 +362,6 @@ my_solution = Solution()
 
 # result = my_solution.climbStairs(45)
 # print (result)
+
+result = my_solution.merge([1,2,3,0,0,0],3,[2,5,6],3)
+print (result)
