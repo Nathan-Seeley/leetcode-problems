f9b8d0ed664723dbb26ef06c2690c34021a7f75f
Nathan-Seeley <wntrstk111@gmail.com>
2023-06-09 21:10:13 -0400

    Finished testing: generate problem solution

--- main.py
+++ main.py
@@ -322,8 +322,29 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
+        output = []
+        for i in range(numRows):
+            if(i == 0):
+                # Create a list to store the prev triangle value for further addition...
+                # Inserting for the first row & store the prev array to the output array...
+                prev = [1]
+                output.append(prev)
+            else:
+                curr = [1]
+                j = 1
+                # Calculate for each of the next values...
+                while(j < i):
+                    # Inserting the addition of the prev arry two values...
+                    curr.append(prev[j-1] + prev[j])
+                    j+=1
+                # Store the number 1...
+                curr.append(1)
+                # Store the value in the Output array...
+                output.append(curr)
+                # Set prev is equal to curr...
+                prev = curr
+        return output 
 
-        
 
 
 my_solution = Solution()
@@ -371,5 +392,8 @@ my_solution = Solution()
 # result = my_solution.climbStairs(45)
 # print (result)
 
-result = my_solution.merge([1,2,3,0,0,0],3,[2,5,6],3)
+# result = my_solution.merge([1,2,3,0,0,0],3,[2,5,6],3)
+# print (result)
+
+result = my_solution.generate(5)
 print (result)
