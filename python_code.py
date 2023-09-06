5587e9c14eda225d4f0f61d36fabee5e833b76d3
Nathan-Seeley <wntrstk111@gmail.com>
2023-09-06 10:36:29 -0400

    Created inital conditional iterating through givin number where result is not equal to one and the sum of squares begans with 0 as a counter

--- python_code.py
+++ python_code.py
@@ -493,8 +493,14 @@ class Solution(object):
         :type n: int
         :rtype: bool
         Keep track of visited numbers
+        Continue loop until number is equal to 1 or stuck in cycle 
+            and set the variable sum of squares equal to zero
+        
         """
         tracker = set()
+        while  n != 1:
+            sum_of_squares = 0
+
 my_solution = Solution()
 
 # result = my_solution.twoSum([2,7,11,15],9)
